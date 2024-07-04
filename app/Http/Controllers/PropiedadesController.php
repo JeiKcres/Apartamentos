eed14cb1be90645cbf4ef3541054d8c1d2e02870
Junior Caceres <jrcaceres61@gmail.com>
2024-07-04 10:54:39 -0600

    nuevos cambios

--- app/Http/Controllers/PropiedadesController.php
+++ app/Http/Controllers/PropiedadesController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Propiedade;
 
 class PropiedadesController extends Controller
 {
-    //
+    public function mostrarPropiedades(){
+        $propiedades = Propiedade::all();
+        return view('mostrarPropiedades', compact('propiedades'));
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DueniosController;
+use App\Http\Controllers\PropiedadesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,5 @@ Route::get('/calcular/{area}/{piso?}', [HomeController::class, 'calcular_V_A']);
 Route::get('/duenios/mostrar', [DueniosController::class, 'mostrarDuenios']);
 Route::get('/duenios/insertar', [DueniosController::class, 'insertarDuenio']);
 Route::post('/duenios/guardar', [DueniosController::class, 'guardarDuenio'])->name('duenio.guardar');
+
+Route::get('/propiedades/mostrar', [PropiedadesController::class, 'mostrarPropiedades']);
